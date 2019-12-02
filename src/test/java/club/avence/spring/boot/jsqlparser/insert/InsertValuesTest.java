986e40a0d7be85d7783f7e010e4fb1abd089f519
c_qiancheng <abcqdsdchina@126.com>
2019-12-02 10:27:42 +0800

    增加单元测试类

--- src/test/java/club/avence/spring/boot/jsqlparser/insert/InsertValuesTest.java
+++ src/test/java/club/avence/spring/boot/jsqlparser/insert/InsertValuesTest.java
@@ -9,7 +9,7 @@ public class InsertValuesTest {
 
     @Test
     public void test() throws Throwable {
-        Statement statement = CCJSqlParserUtil.parse("insert into hm_fnd_tpa values (seq_hm_fnd_tpa_id.nextval, 1, 1, 'hello')");
+        Statement statement = CCJSqlParserUtil.parse("insert into hm_fnd_tpa values (seq_hm_fnd_tpa_id.nextval, 1, 1, 'hello', sysdate)");
         Insert insertStatement = (Insert) statement;
         System.out.println(insertStatement);
     }
